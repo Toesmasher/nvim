ab0bab47e9fee1b7ab8ca20550dbb2f880831090
Niklas Berggren <nicke@toesmasher.se>
2024-07-08 13:29:53 +0200

    Add prettier formatter

--- lua/plugin-settings/lsp/server-settings/none-ls.lua
+++ lua/plugin-settings/lsp/server-settings/none-ls.lua
@@ -3,6 +3,11 @@ if not status_ok then
   return
 end
 
-none_ls.setup({
+local common = require("plugin-settings.lsp.server-settings.common_functions")
 
+none_ls.setup({
+  sources = {
+    none_ls.builtins.formatting.prettier
+  },
+  on_attach = common.default_attach
 })
